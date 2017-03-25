ff2ab6c91a37e82e2a262d7cacb0ebad4a0f76f3
sumit <sgupta40@sapient.com>
2017-03-25 08:43:09 +0530

    code

--- ci/tasks/build.sh
+++ ci/tasks/build.sh
@@ -2,14 +2,7 @@
 
 set -e -x
 
-pwd
-echo pwd
-echo "$USER"
-_user="$(id -u -n)"
-_uid="$(id -u)"
-echo "User name : $_user"
-echo "User name ID (UID) : $_uid"
+
 cd decoders
-echo "test"
-gradle build
-echo "test2"
+gradle -v
+gradle test
